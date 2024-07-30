ae3e473247ec3024f31296ce6d51921b032847e3
theEast17 <nagarpoorv2001@gmail.com>
2024-07-30 18:51:41 +0530

    fix brands bug

--- controller/Product.js
+++ controller/Product.js
@@ -13,40 +13,21 @@ export const createProduct = async (req, res) => {
 
 export const fetchAllProduct = async (req, res) => {
 
-    // req.params: Contains route parameters (in the path portion of the URL) as key-value pairs.
-    // req.query: Contains the URL query string parameters as key-value pairs.
-    // req.body: Contains the parsed body of the request (for example, form data or JSON).
-    // req.headers: Contains the headers sent by the client.
-    // req.method: Contains the HTTP method of the request (e.g., GET, POST).
-    // req.url: Contains the full URL of the request.
-    // req.path: Contains the path portion of the URL.
-
     let query = ProductSchema.find({});   //all the products 
     let totalProductsQuery = ProductSchema.find({});  // all the products
 
 
     if (req.query.category) {
-        query = query.find({ category: req.query.category });
-        totalProductsQuery = totalProductsQuery.find({ category: req.query.category });
+        query = query.find({ category: { $in: req.query.category.split(',') } });
+        totalProductsQuery = totalProductsQuery.find({ category: { $in: req.query.category.split(',') } });
     }
 
     if (req.query.brand) {
-        query = query.find({ brand: req.query.brand });
-        totalProductsQuery = totalProductsQuery.find({ brand: req.query.brand });
+        query = query.find({ brand: { $in: req.query.brand.split(',') } });
+        totalProductsQuery = totalProductsQuery.find({ brand: { $in: req.query.brand.split(',') } });
     }
 
     if (req.query._sort && req.query._order) {
-        // The square brackets are used in this context to dynamically set the property name of an object.
-        // This is a feature called computed property names in JavaScript, which allows you to use an expression to determine the property name.
-
-        // Dynamic Property Names:
-
-        // When you write { [req.query._sort]: req.query._order }, it dynamically creates an object where the key is the value of req.query._sort and the value is the value of req.query._order.
-        // This is useful because the sort field (req.query._sort) is not known until runtime. It could be any field in your schema, like price, name, rating, etc.
-        // Without Computed Property Names:
-
-        // If you were to write it without the square brackets, like { req.query._sort: req.query._order }, JavaScript would interpret req.query._sort as a literal string key, which is not what you want.
-
         query = query.sort({ [req.query._sort]: req.query._order });
     }
 
